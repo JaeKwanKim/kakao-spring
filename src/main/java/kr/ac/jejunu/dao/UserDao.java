4caf5383020b30229927440a478dc4357b6567c7
JaeKwanKim <jk79thor@icloud.com>
2016-04-01 16:41:03 +0900

    4.1일자 add() 추가 및 리펙토링

--- src/main/java/kr/ac/jejunu/dao/UserDao.java
+++ src/main/java/kr/ac/jejunu/dao/UserDao.java
@@ -11,8 +11,7 @@ public class UserDao {
     public User get(long id) throws SQLException, ClassNotFoundException {
         String sql = "select * from test where id = ?";
 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false","root","");
+        Connection connection = getConnection();
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setLong(1, id);
 
@@ -29,5 +28,41 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+
+    public long add(User user) throws SQLException, ClassNotFoundException {
+        String sql = "insert into test(name, password) values(?,?)";
+
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, user.getName());
+        statement.setString(2, user.getPassword());
+
+        statement.executeUpdate();
+
+        long id = getLastInsertId(connection);
+
+        statement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private  long getLastInsertId(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("select last_insert_id()");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+
+        return id;
+    }
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false","root","");
     }
 }
