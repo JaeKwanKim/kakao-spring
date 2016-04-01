55236a3225d5b60243f0c8c70fc9454321fd5e5c
JaeKwanKim <jk79thor@icloud.com>
2016-04-01 16:17:41 +0900

    4.1일자 add() TestCase

--- src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -14,21 +14,40 @@ public class UserDaoTest {
     long id;
     String name;
     String password;
+    UserDao userDao;
 
     @Before
     public void setup() {
         id = 1L;
         name = "김재관";
         password = "1234";
+        userDao = new UserDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
         User user = userDao.get(id);
 
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        String name = "헐크";
+        String password = "2222";
+
+        user.setName(name);
+        user.setPassword(password);
+
+        long id = userDao.add(user);
+
+        User resultUser = userDao.get(id);
+
+        assertThat(resultUser.getId(), is(id));
+        assertThat(resultUser.getName(), is(name));
+        assertThat(resultUser.getPassword(), is(password));
     }
 }
